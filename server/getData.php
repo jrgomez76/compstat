d4779fbd601e9894b0f34161e053d5a55315e0d8
George <george.vakakis@gmal.com>
2016-03-23 19:19:30 -0400

    New API Key

--- server/getData.php
+++ server/getData.php
@@ -2,7 +2,7 @@
 
 function getData($type, $queryString) {
 //$api_key = '3OlR5AKFDPhrgSToQWqsa6JupmU1fZbc';
-$api_key = '58ipm4TLHYoGhyeZ0NA2ngbW1PrafjcI';
+$api_key = 'syjEFeRlMzrxd2oQSYcPJ6IvC8T4WgbD';
 $url = 'http://api.probasketballapi.com/' . $type;
 $query_string = 'api_key=' . $api_key . $queryString;
 
